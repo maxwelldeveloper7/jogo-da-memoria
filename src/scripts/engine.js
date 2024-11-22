a8dd7caba2ddd6a5f9975c301af12d0683264fe7
Maxwell de Oliveira Chaves <maxwellchaves1844@gmail.com>
2024-11-22 10:36:29 -0300

    Dando efeito de rotate nos cards

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -18,10 +18,25 @@ let openCards = [];
 
 let shuffleEmojis = emojis.sort(() => (Math.random() > 0.5 ? 2 : -1));
 
-
 for (let i = 0; i < emojis.length; i++) {
     let box = document.createElement("div");
     box.className = "item";
     box.innerHTML = shuffleEmojis[i];
+    box.onclick = handleClick;
     document.querySelector(".game").appendChild(box);
+}
+
+function handleClick(){
+    if(openCards.length < 2){
+        this.classList.add('boxOpen');
+        openCards.push(this)
+    }
+
+    if(openCards.length == 2){
+        setTimeout(checkMatch, 500);
+    }
+}
+
+function checkMatch(){
+    
 }
